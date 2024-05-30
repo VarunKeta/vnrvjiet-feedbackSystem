cde2a93da87a7e3b43e59abe59e4a470d05842df
varun <varunketa2004@gmail.com>
2024-05-30 16:22:57 +0530

    commit-1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+//import './App.css';
+import Alumini from './components/forms/Alumini';
+import Faculty from './components/forms/Faculty';
+import Industry from './components/forms/Industry';
+
+function App() {
+  return (
+    <div className="App">
+      
+          <Alumini/>
+          <Industry/>
+          <Faculty/>
+    </div>
+  );
+}
+
+export default App;
