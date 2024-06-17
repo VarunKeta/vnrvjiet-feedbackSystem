d381ebf7ba83dec83183c12ba4fafa8b00565ac7
aashritharaj26 <141063242+aashritharaj26@users.noreply.github.com>
2024-06-17 22:16:42 +0530

    Delete server directory

--- server/APIs/admin-api.js
+++
@@ -1,9 +0,0 @@
-const exp=require('express')
-const adminApp=exp.Router()
-const bcryptjs=require('bcryptjs')
-const expressAsyncHandler=require('express-async-handler')
-
-//create-user
-adminApp.post('/create-user',expressAsyncHandler(async(req,res)=>{
-    
-}))
