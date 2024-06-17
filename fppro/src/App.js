b5e4a2e0dc83afc713b40f3db7454a52636a3b55
aashritharaj26 <141063242+aashritharaj26@users.noreply.github.com>
2024-06-17 22:51:33 +0530

    Update App.js

--- fppro/src/App.js
+++ fppro/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Alumini from './Components/Forms/Alumini/Alumini';
 import 'bootstrap/dist/css/bootstrap.css';
